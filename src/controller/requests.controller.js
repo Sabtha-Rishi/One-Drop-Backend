5f8c0743049af8409196108e3d5d68dbbbc458e4
Sabtha Rishi <sabtharishi01@gmail.com>
2022-08-03 14:28:43 +0530

    New request route and control

--- src/controller/requests.controller.js
+++ src/controller/requests.controller.js
@@ -1,8 +1,12 @@
 // IMPORTS
-const RequestModel = require("../model/requests.model                                                                                                                                                                                                                                                                                                                                                                        ");
+const RequestModel = require("../model/requests.model");
+const jwt = require("jsonwebtoken");
+
+JWT_SECRET = process.env.JWT_SECRET;
 
 //Request Handlers
 
+//All Requests
 const allRequests = (req, res) => {
   RequestModel.find({}, (err, results) => {
     if (err) {
@@ -18,5 +22,30 @@ const allRequests = (req, res) => {
   });
 };
 
-const RequestController = { allRequests };
+// New Request
+const create = (req, res) => {
+  const userID = jwt.verify(req.cookies.token, JWT_SECRET).id;
+
+  let newReq = new RequestModel(req.body);
+
+  newReq.userId = userID;
+
+  newReq.save((err, result) => {
+    if (err) {
+      return res.json({
+        isSuccess: false,
+        err: err.message,
+      });
+    }
+    return res.status(201).json({
+      isSuccess: true,
+      request: result,
+    });
+  });
+};
+
+const RequestController = {
+  allRequests,
+  create,
+};
 module.exports = RequestController;
--- src/router/requests.router.js
+++ src/router/requests.router.js
@@ -1,18 +1,16 @@
-const express = require('express');
+const express = require("express");
 
-const RequestsController = require('../controller/products.controller');
-const Validator = require('../middleware/validator.middleware');
+const RequestsController = require("../controller/requests.controller");
+const Validator = require("../middleware/validator.middleware");
 
 const RequestRouter = express.Router();
 
 //Sub Routes [/requests]
-RequestRouter.get("/", RequestsController.allrequests);
+RequestRouter.get("/", RequestsController.allRequests);
 
-// RequestRouter.post('/create', RequestsController.upload.single('file') ,RequestsController.create)
+RequestRouter.post("/new", RequestsController.create);
 // RequestRouter.delete('/:reqID/delete', Validator.isLoggedin,RequestsController.deleteRequest)
 // RequestRouter.get("/:reqID", RequestsController.getRequest);
-
-
 
 //Exports
 module.exports = RequestRouter;
