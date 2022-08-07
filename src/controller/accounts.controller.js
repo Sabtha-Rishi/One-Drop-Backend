c4060d29f28e81f575d0b3462be85f47f8611625
Sabtha Rishi <sabtharishi01@gmail.com>
2022-08-07 16:01:51 +0530

    Trying to fix cookies issue

--- src/controller/accounts.controller.js
+++ src/controller/accounts.controller.js
@@ -76,6 +76,7 @@ const loginUser = (req, res) => {
         });
         return res.status(201).send({
           isAuthenticated: true,
+          new: true,
         });
       } else {
         return res.status(400).send({
